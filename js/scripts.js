9f515cd2beb07bba9988245acdc9b59457973df2
Bart Webber and Esti Shay <esti@>
2017-07-10 13:55:31 -0700

    send intputs to an array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
 $(document).ready(function() {
-  var groceries = [];
+
 
   $("#shoppingForm").submit(function(event) {
     // groceries.push($("input#item").val());
+    var groceries = [];
+
     var itemInput = $("input#item").val();
 
-    $("ul").append("<li>" + itemInput + "</li>")
+    if (itemInput === '') {
+      alert("Please enter an item name.")
+    } else {
+      groceries.push($("input#item").val());
+      $("p").append(groceries);
+    }
 
     event.preventDefault();
   });
 
 
+
+// var itemInput = $("input#item").val();
+  //  $("ul").append("<li>" + itemInput + "</li>")
 
   // groceries.forEach(function(grocery) {
   //   $("ul").append("<li>"+grocery+"</li>");
